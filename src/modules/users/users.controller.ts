9bccd3692a209ff353a837da5010170505054db0
Zhdanovich <Zhiv80@mail.ru>
2025-06-26 11:30:20 +0300

    Feat: add CRUD operations, DTOs

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,22 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   HttpCode,
   HttpException,
   HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
   Post,
+  Put,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user/create-user-dto';
-import { User } from './entities/user.entity';
+import { UserDTO } from './dto/user/user-dto';
+import { UpdatePropertiesUserDTO } from './dto/update-properties-user/updateUserPropertiesDto';
 
 @Controller('users')
 export class UsersController {
@@ -16,7 +24,7 @@ export class UsersController {
 
   @Post()
   @HttpCode(201)
-  async create(@Body() user: CreateUserDTO): Promise<User | void> {
+  async create(@Body() user: CreateUserDTO): Promise<UserDTO | void> {
     try {
       return await this.usersService.create(user);
     } catch (error) {
@@ -27,5 +35,48 @@ export class UsersController {
         );
       }
     }
+  }
+
+  @Get()
+  getUsers(): Promise<UserDTO[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+  ): Promise<UserDTO> {
+    console.log(`REQUEST ID ${req?.headers['x-request-id']}`);
+    console.log(`LOCALE IS ${req['locale']}`);
+    return this.usersService.findOneById(id);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Body() user: CreateUserDTO,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<CreateUserDTO> {
+    return this.usersService.update(id, user);
+  }
+
+  @Patch(':id')
+  updateUserProperties(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() userProperties: UpdatePropertiesUserDTO,
+  ) {
+    return this.usersService.update(id, userProperties);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
+  }
+
+  @Delete('softDelete/:id')
+  @HttpCode(204)
+  softDeleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.softRemove(id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersRepository } from './repositories/users.repository';
 import { User } from './entities/user.entity';
+import { Role } from 'src/enums/role.enum';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: UsersRepository,
+  ) {}
+
   create(userData: Partial<User>): Promise<User> {
     const user = this.usersRepository.create(userData);
     return this.usersRepository.save(user);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
   async findOneById(id: number): Promise<User> {
@@ -24,5 +34,30 @@ export class UsersService {
       throw new NotFoundException(`User with email ${email} not found`);
     }
     return user;
+  }
+
+  async update(id: number, updateData: Partial<User>): Promise<User> {
+    const user = await this.findOneById(id);
+    Object.assign(user, updateData);
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number): Promise<void> {
+    const user = await this.findOneById(id);
+    await this.usersRepository.remove(user);
+  }
+
+  async softRemove(id: number): Promise<void> {
+    await this.usersRepository.softDelete(id);
+  }
+
+  async restore(id: number): Promise<void> {
+    await this.usersRepository.restore(id);
+  }
+
+  async updateUserRole(id: number, newRole: Role): Promise<User> {
+    const user = await this.findOneById(id);
+    user.role = [newRole];
+    return this.usersRepository.save(user);
   }
 }
