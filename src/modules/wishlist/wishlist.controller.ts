43545ebbe2b715d294165a85cf4e2db86419caac
Zhdanovich <Zhiv80@mail.ru>
2025-09-15 16:19:19 +0300

    feat: add wishlist controller

--- src/modules/wishlist/wishlist.controller.ts
+++ src/modules/wishlist/wishlist.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Param, Post, Request } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 
 @Controller('wishlist')
@@ -6,8 +14,26 @@ export class WishlistController {
   constructor(private readonly wishlistService: WishlistService) {}
 
   @Post(':productId')
-  addToWishlist(@Request() req, @Param('productId') productId: number) {
+  addToWishlist(
+    @Request() req,
+    @Param('productId', ParseIntPipe) productId: number,
+  ) {
     const userId: number = req.user.id;
     return this.wishlistService.addToWishlist(userId, productId);
+  }
+
+  @Delete('productId')
+  removeFromWishlist(
+    @Request() req,
+    @Param('productId', ParseIntPipe) productId: number,
+  ) {
+    const userId: number = req.user.id;
+    return this.wishlistService.removeFromWishlist(userId, productId);
+  }
+
+  @Get()
+  viewWishlist(@Request() req) {
+    const userId: number = req.user.id;
+    return this.wishlistService.viewWishlist(userId);
   }
 }
